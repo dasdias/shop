c839d64936830c49e20209be4bca2f9fb528e181
Alex Fix <dasdias@yandex.ru>
2022-03-04 22:10:47 +0300

    shop

---
+++ src/js/script.js
@@ -0,0 +1,19 @@
+// 'use strict';
+// document.addEventListener('DOMContentLoaded', function () {
+//     const disp = 'Hello';
+//     const mass = ['name', 2, 'Vasya', 7]
+//     // console.log('777');
+//     // console.log(disp);
+
+//     const test = (mass) => {
+//         mass.forEach(element => {
+//             if (typeof(element) === 'string') {
+//                 console.log(element);                
+//             }
+//         });
+
+//     }
+
+//     test(mass);
+
+// });
