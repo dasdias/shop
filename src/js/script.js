dcf291fd2e6c0e06eb426e2ed19a45c9da4d5be7
Alex Fix <dasdias@yandex.ru>
2022-03-23 00:17:05 +0300

    develop block filter

--- src/js/script.js
+++ src/js/script.js
@@ -1,22 +1,43 @@
 'use strict';
 document.addEventListener('DOMContentLoaded', function () {
-    const menuCover = document.querySelector('.menu-cover');
-    const headerBurger = document.querySelector('.header-burger');
-    const menuClose = document.querySelector('.menu__close');
+	const menuCover = document.querySelector('.menu-cover');
+	const headerBurger = document.querySelector('.header-burger');
+	const menuClose = document.querySelector('.menu__close');
 
-    const openMenu = () => {
-        menuCover.classList.toggle('menu-cover-hidden');
-    }
-    const closeMenu = () => {
-        menuCover.classList.add('menu-cover-hidden');
-    }
+	const openMenu = () => {
+		menuCover.classList.toggle('menu-cover-hidden');
+	}
+	const closeMenu = () => {
+		menuCover.classList.add('menu-cover-hidden');
+	}
 
-    headerBurger.addEventListener('click', openMenu);
-    menuClose.addEventListener('click', closeMenu);
-    menuCover.addEventListener('click', (e) => {
-        let target = e.target;
-        if (target.className === 'menu-cover') {
-            closeMenu();
-        }       
-    });
+	headerBurger.addEventListener('click', openMenu);
+	menuClose.addEventListener('click', closeMenu);
+	menuCover.addEventListener('click', (e) => {
+		let target = e.target;
+		if (target.className === 'menu-cover') {
+				closeMenu();
+		}       
+	});
+
+
+	const filterCategorys = document.querySelectorAll('.filter-category__title');
+
+	function tabs(currentElem) {
+		filterCategorys.forEach(elem => {
+			elem.nextElementSibling.classList.remove('filter-category__group--active');
+		});
+		currentElem.classList.add('filter-category__group--active');
+	}
+
+	filterCategorys.forEach(element => {
+		element.addEventListener('click', (e) => {
+			const target = e.target;
+			console.log(target.nextElementSibling);
+			const nextElem = target.nextElementSibling;
+			tabs(nextElem);
+			// nextElem.classList.toggle('filter-category__group--active');
+			// console.log(nextElem.clientHeight); 
+		});
+	});
 });
